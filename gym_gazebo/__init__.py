f94cf7adf650b54e9a77b4f4d6954a992844049b
Saurabh Nair <saurabh.nk@gmail.com>
2017-11-28 10:00:41 -0800

    Removing other env calls

--- gym_gazebo/__init__.py
+++ gym_gazebo/__init__.py
@@ -6,59 +6,6 @@ logger = logging.getLogger(__name__)
 # Gazebo
 # ----------------------------------------
 
-# Turtlebot envs
-register(
-    id='GazeboMazeTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs:GazeboMazeTurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuitTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs:GazeboCircuitTurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2TurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs:GazeboCircuit2TurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2TurtlebotLidarNn-v0',
-    entry_point='gym_gazebo.envs:GazeboCircuit2TurtlebotLidarNnEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2cTurtlebotCameraNnEnv-v0',
-    entry_point='gym_gazebo.envs:GazeboCircuit2cTurtlebotCameraNnEnv',
-    # More arguments here
-)
-register(
-    id='GazeboRoundTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs:GazeboRoundTurtlebotLidarEnv',
-    # More arguments here
-)
-
-# Erle-Copter envs
-register(
-    id='GazeboErleCopterHover-v0',
-    entry_point='gym_gazebo.envs:GazeboErleCopterHoverEnv',
-)
-
-#Erle-Rover envs
-register(
-    id='GazeboMazeErleRoverLidar-v0',
-    entry_point='gym_gazebo.envs:GazeboMazeErleRoverLidarEnv',
-)
-register(
-    id='GazeboCircuit2SnakeMonsterLidar-v0',
-    entry_point='gym_gazebo.envs:GazeboCircuit2SnakeMonsterLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboSnakeLidar-v0',
-    entry_point='gym_gazebo.envs:GazeboSnakeLidarEnv',
-    # More arguments here
-)
 
 
 register(
@@ -67,8 +14,3 @@ register(
     # More arguments here
 )
 
-register(
-    id='GazeboCircuit2cSnakeMonsterCameraNnEnv-v0',
-    entry_point='gym_gazebo.envs:GazeboCircuit2cSnakeMonsterCameraNnEnv',
-    # More arguments here
-)

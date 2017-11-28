8760d1c3d40b590b16a6ca71c0ae0616cd5d7a9a
Hadi Salman <hadicsalman@gmail.com>
2017-11-28 14:27:09 -0500

    Better rewerd function for lidar dqn for turtlebot

--- gym_gazebo/envs/gazebo_circuit2_turtlebot_lidar_nn.py
+++ gym_gazebo/envs/gazebo_circuit2_turtlebot_lidar_nn.py
@@ -8,9 +8,7 @@ from gym import utils, spaces
 from gym_gazebo.envs import gazebo_env
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty
-
 from sensor_msgs.msg import LaserScan
-
 from gym.utils import seeding
 
 class GazeboCircuit2TurtlebotLidarNnEnv(gazebo_env.GazeboEnv):
@@ -22,9 +20,10 @@ class GazeboCircuit2TurtlebotLidarNnEnv(gazebo_env.GazeboEnv):
         self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
         self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        self.action_space = spaces.Discrete(3) #F,L,R
 
         self.reward_range = (-np.inf, np.inf)
-
+        self.observation_space = spaces.Box(-0, 7,(20,))
         self._seed()
 
     def calculate_observation(self,data):
@@ -46,13 +45,34 @@ class GazeboCircuit2TurtlebotLidarNnEnv(gazebo_env.GazeboEnv):
         except rospy.ServiceException, e:
             print ("/gazebo/unpause_physics service call failed")
 
-        max_ang_speed = 0.3
-        ang_vel = (action-10)*max_ang_speed*0.1 #from (-0.33 to + 0.33)
+        # max_ang_speed = 0.3
+        # ang_vel = (action-10)*max_ang_speed*0.1 #from (-0.33 to + 0.33)
 
-        vel_cmd = Twist()
-        vel_cmd.linear.x = 0.2
-        vel_cmd.angular.z = ang_vel
-        self.vel_pub.publish(vel_cmd)
+        # vel_cmd = Twist()
+        # vel_cmd.linear.x = 0.2
+        # vel_cmd.angular.z = ang_vel
+        # self.vel_pub.publish(vel_cmd)
+
+        if action == 0: #FORWARD
+            vel_cmd = Twist()
+            
+            vel_cmd.linear.x = 0.3
+            vel_cmd.angular.z = 0.0
+            self.vel_pub.publish(vel_cmd)
+        elif action == 1: #LEFT
+            vel_cmd = Twist()
+            
+            vel_cmd.linear.x = 0.05
+            vel_cmd.angular.z = 0.3
+           
+            self.vel_pub.publish(vel_cmd)
+        elif action == 2: #RIGHT
+            vel_cmd = Twist()
+           
+            vel_cmd.linear.x = 0.05
+            vel_cmd.angular.z = -0.3
+            
+            self.vel_pub.publish(vel_cmd)
 
         data = None
         while data is None:
@@ -70,13 +90,21 @@ class GazeboCircuit2TurtlebotLidarNnEnv(gazebo_env.GazeboEnv):
 
         state,done = self.calculate_observation(data)
 
+        # if not done:
+        #     # Straight reward = 5, Max angle reward = 0.5
+        #     reward = round(15*(max_ang_speed - abs(ang_vel) +0.0335), 2)
+        #     # print ("Action : "+str(action)+" Ang_vel : "+str(ang_vel)+" reward="+str(reward))
+        # else:
+        #     reward = -200
         if not done:
-            # Straight reward = 5, Max angle reward = 0.5
-            reward = round(15*(max_ang_speed - abs(ang_vel) +0.0335), 2)
-            # print ("Action : "+str(action)+" Ang_vel : "+str(ang_vel)+" reward="+str(reward))
+            if action == 0:
+                reward = 5
+            else:
+                reward = 1
         else:
             reward = -200
 
+        # embed()
         return np.asarray(state), reward, done, {}
 
     def _reset(self):
